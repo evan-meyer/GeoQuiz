42233c106152df4f81cafa335f391bded46e357d
evmeyer <evan-meyer@uiowa.edu>
2020-01-22 17:39:12 -0600

    added getter and setters for Question member variables

--- app/src/main/java/com/bignerdranch/android/geoquiz/Question.java
+++ app/src/main/java/com/bignerdranch/android/geoquiz/Question.java
@@ -2,6 +2,22 @@ package com.bignerdranch.android.geoquiz;
 
 public class Question {
 
+    public int getTextResId() {
+        return mTextResId;
+    }
+
+    public void setTextResId(int textResId) {
+        mTextResId = textResId;
+    }
+
+    public boolean isAnswerTrue() {
+        return mAnswerTrue;
+    }
+
+    public void setAnswerTrue(boolean answerTrue) {
+        mAnswerTrue = answerTrue;
+    }
+
     private int mTextResId;
     private boolean mAnswerTrue;
 
