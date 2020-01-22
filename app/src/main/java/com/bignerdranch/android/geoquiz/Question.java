f52934119ab03dc2b9d2ed91ab33d6c2482bfe95
evmeyer <evan-meyer@uiowa.edu>
2020-01-22 17:29:27 -0600

    added constructor for Question and member variables

--- app/src/main/java/com/bignerdranch/android/geoquiz/Question.java
+++ app/src/main/java/com/bignerdranch/android/geoquiz/Question.java
@@ -1,4 +1,12 @@
 package com.bignerdranch.android.geoquiz;
 
 public class Question {
+
+    private int mTextResId;
+    private boolean mAnswerTrue;
+
+    public Question(int textResId, boolean answerTrue) {
+        mTextResId = textResId;
+        mAnswerTrue = answerTrue;
+    }
 }
